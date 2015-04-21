12b138ee90237cb8d11b556cd227df947739d38b
AdriVanHoudt <adri.van.houdt@gmail.com>
2015-04-21 10:36:01 +0200

    use internal eslintrc from lab // update styles accordingly

--- test/index.js
+++ test/index.js
@@ -8,182 +8,182 @@ var it = lab.it;
 var expect = Code.expect;
 
 var dbOptions = {
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'test'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
 };
 
 lab.experiment('Hapi MySQL', function () {
 
-	lab.experiment('Basics', function () {
+    lab.experiment('Basics', function () {
 
-		it('Makes a db connection that works', function (done) {
+        it('Makes a db connection that works', function (done) {
 
-			var options = Hoek.clone(dbOptions);
+            var options = Hoek.clone(dbOptions);
 
-			var server = new Hapi.Server();
-			server.connection();
+            var server = new Hapi.Server();
+            server.connection();
 
-			server.register({
-				register: require('../'),
-				options: options
-			}, function (err) {
+            server.register({
+                register: require('../'),
+                options: options
+            }, function (err) {
 
-				expect(err).to.not.exist();
+                expect(err).to.not.exist();
 
-				server.route([{
-					method: 'POST',
-					path: '/test',
-					config: {
-						handler: function (request, reply) {
+                server.route([{
+                    method: 'POST',
+                    path: '/test',
+                    config: {
+                        handler: function (request, reply) {
 
-							var sql = 'INSERT INTO test SET id = 1';
+                            var sql = 'INSERT INTO test SET id = 1';
 
-							expect(request.app.db, 'db connection').to.exist();
+                            expect(request.app.db, 'db connection').to.exist();
 
-							request.app.db.query(sql, function (err, results) {
+                            request.app.db.query(sql, function (err, results) {
 
-								expect(err, 'error').to.not.exist();
-								expect(results.insertId, 'insert Id').to.exist();
+                                expect(err, 'error').to.not.exist();
+                                expect(results.insertId, 'insert Id').to.exist();
 
-								return reply(results.affectedRows);
-							});
-						}
-					}
-				}, {
-					method: 'GET',
-					path: '/test',
-					config: {
-						handler: function (request, reply) {
+                                return reply(results.affectedRows);
+                            });
+                        }
+                    }
+                }, {
+                    method: 'GET',
+                    path: '/test',
+                    config: {
+                        handler: function (request, reply) {
 
-							var sql = 'SELECT * FROM test';
+                            var sql = 'SELECT * FROM test';
 
-							expect(request.app.db, 'db connection').to.exist();
+                            expect(request.app.db, 'db connection').to.exist();
 
-							request.app.db.query(sql, function (err, results) {
+                            request.app.db.query(sql, function (err, results) {
 
-								expect(err, 'error').to.not.exist();
+                                expect(err, 'error').to.not.exist();
 
-								return reply(results);
-							});
-						}
-					}
-				}]);
+                                return reply(results);
+                            });
+                        }
+                    }
+                }]);
 
-				server.inject({
-					method: 'POST',
-					url: '/test'
-				}, function (response) {
+                server.inject({
+                    method: 'POST',
+                    url: '/test'
+                }, function (response) {
 
-					expect(response.statusCode, 'post status code').to.equal(200);
-					expect(response.result, 'post result').to.be.above(0);
+                    expect(response.statusCode, 'post status code').to.equal(200);
+                    expect(response.result, 'post result').to.be.above(0);
 
-					server.inject({
-						method: 'GET',
-						url: '/test'
-					}, function (response) {
+                    server.inject({
+                        method: 'GET',
+                        url: '/test'
+                    }, function (response) {
 
-						expect(response.statusCode, 'get status code').to.equal(200);
-						expect(response.result.length, 'get result').to.be.above(0);
+                        expect(response.statusCode, 'get status code').to.equal(200);
+                        expect(response.result.length, 'get result').to.be.above(0);
 
-						server.stop(done);
-					});
-				});
-			});
-		});
+                        server.stop(done);
+                    });
+                });
+            });
+        });
 
-		it('Quite fail when connection is deleted', function (done) {
+        it('Quite fail when connection is deleted', function (done) {
 
-			var options = Hoek.clone(dbOptions);
+            var options = Hoek.clone(dbOptions);
 
-			var server = new Hapi.Server();
-			server.connection();
+            var server = new Hapi.Server();
+            server.connection();
 
-			server.register({
-				register: require('../'),
-				options: options
-			}, function (err) {
+            server.register({
+                register: require('../'),
+                options: options
+            }, function (err) {
 
-				expect(err).to.not.exist();
+                expect(err).to.not.exist();
 
-				server.route([{
-					method: 'GET',
-					path: '/test',
-					config: {
-						handler: function (request, reply) {
+                server.route([{
+                    method: 'GET',
+                    path: '/test',
+                    config: {
+                        handler: function (request, reply) {
 
-							request.app.db = undefined;
-							return reply('ok');
-						}
-					}
-				}]);
+                            request.app.db = undefined;
+                            return reply('ok');
+                        }
+                    }
+                }]);
 
-				server.inject({
-					method: 'GET',
-					url: '/test'
-				}, function (response) {
+                server.inject({
+                    method: 'GET',
+                    url: '/test'
+                }, function (response) {
 
-					expect(response.statusCode, 'post status code').to.equal(200);
-					expect(response.result, 'post result').to.equal('ok');
+                    expect(response.statusCode, 'post status code').to.equal(200);
+                    expect(response.result, 'post result').to.equal('ok');
 
-					server.stop(done);
-				});
-			});
-		});
+                    server.stop(done);
+                });
+            });
+        });
 
-		it('Pool is set to null on Server.stop()', function (done) {
+        it('Pool is set to null on Server.stop()', function (done) {
 
-			var options = Hoek.clone(dbOptions);
+            var options = Hoek.clone(dbOptions);
 
-			var server = new Hapi.Server();
-			server.connection();
+            var server = new Hapi.Server();
+            server.connection();
 
-			server.register({
-				register: require('../'),
-				options: options
-			}, function (err) {
+            server.register({
+                register: require('../'),
+                options: options
+            }, function (err) {
 
-				expect(err).to.not.exist();
+                expect(err).to.not.exist();
 
-				server.start(function(err) {
+                server.start(function(err) {
 
-					expect(err).to.not.exist();
+                    expect(err).to.not.exist();
 
-					server.stop(function() {
+                    server.stop(function() {
 
-						setImmediate(done);
-					});
-				});
-			});
-		});
-	});
+                        setImmediate(done);
+                    });
+                });
+            });
+        });
+    });
 
-	lab.experiment('Extras', function () {
+    lab.experiment('Extras', function () {
 
-		it('Exposes getDb on the server', function (done) {
+        it('Exposes getDb on the server', function (done) {
 
-			var options = Hoek.clone(dbOptions);
+            var options = Hoek.clone(dbOptions);
 
-			var server = new Hapi.Server();
-			server.connection();
+            var server = new Hapi.Server();
+            server.connection();
 
-			server.register({
-				register: require('../'),
-				options: options
-			}, function (err) {
+            server.register({
+                register: require('../'),
+                options: options
+            }, function (err) {
 
-				expect(err).to.not.exist();
-				expect(server.getDb, 'getDb').to.exist();
+                expect(err).to.not.exist();
+                expect(server.getDb, 'getDb').to.exist();
 
-				server.getDb(function (err, db) {
+                server.getDb(function (err, db) {
 
-					expect(err).to.not.exist();
-					expect(db, 'db').to.exist();
+                    expect(err).to.not.exist();
+                    expect(db, 'db').to.exist();
 
-					server.stop(done);
-				});
-			});
-		});
-	});
+                    server.stop(done);
+                });
+            });
+        });
+    });
 });
